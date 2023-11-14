75abbcdca0533aeda65b7062f7faf46b30e545bd
GAMG-94 <pro.potest@gmail.com>
2023-11-14 12:27:13 -0500

    Creacion de proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    ".pages/**/*.{html,ts}",
+    ".components/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
